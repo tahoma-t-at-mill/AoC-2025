5f6c7e8034b781287fe55f51da899601a0090e2e
Tahoma Toelkes <tahoma.t@chewielabs.com>
2025-12-02 07:34:41 -0800

    day 1 part 2

--- day/1/code.py
+++ day/1/code.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# <https://adventofcode.com/2025/day/1>
+
 class Combination:
 
     def __init__(self, value : int = 0, position : int = 50, modulus : int = 100):
@@ -7,10 +9,10 @@ class Combination:
         self.position = position % modulus
         self.password = abs(value)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.password} ({self.position} / {self.modulus})"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Combination(password = {self.password}, position = {self.position}, modulus = {self.modulus})"
 
     def left(self, ticks : int) -> int:
@@ -26,7 +28,7 @@ class Combination:
             self.password += 1
         return self.position
 
-    def parse(self, cmd : str) -> int:
+    def parse(self, cmd : str, verbose : bool = False) -> int:
         cmd = cmd.strip()
         direction = cmd[0]
         distance = cmd[1:]
@@ -36,56 +38,78 @@ class Combination:
         elif direction == 'R':
             if distance.isdigit():
                 self.right(int(distance))
+        if verbose:
+            print(f"{cmd} -> {self}")
         return self.position
 
     def read(self, filename : str, verbose : bool = False) -> int:
         with open(filename, 'r') as f:
             for line in f:
-                self.parse(line)
-                if verbose:
-                    print(f"{self}")
+                self.parse(line, verbose)
         return self.position
 
 
 def test1() -> bool:
     c1 = Combination()
-    print(f"c1 = {repr(c1)}")
     for n in [-50, -1, -9, 40]:
         m = c1.rotate(n)
-        print(f"c1.rotate({n}) -> {m};\tc1 = {c1}")
     return c1.password == 1 and c1.position == 30 and c1.modulus == 100
 
 def test2() -> bool:
     c2 = Combination(modulus = 50)
-    print(f"c2 = {repr(c2)}")
     for cmd in ["L1", " L9", "foo", "R-10", "L 5", "bar", "L40 ", "R0"]:
         m = c2.parse(cmd)
-        print(f"c2.parse(\"{cmd}\") -> {m};\tc2 = {c2}")
     return c2.password == 2 and c2.position == 0 and c2.modulus == 50
 
 def test3() -> bool:
     c3 = Combination()
-    print(f"c3 = {repr(c3)}")
     with open("test3.txt", 'w') as f:
         for cmd in ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]:
             f.write(cmd)
             f.write("\n")
-    m = c3.read("test3.txt", verbose = True)
-    print(f"c3.read(\"test3.txt\", verbose = True) -> {m};\tc3 = {c3}")
+    m = c3.read("test3.txt")
     return c3.password == 3 and c3.position == 32 and c3.modulus == 100
-
-#print(f"test1() -> {test1()}")
-#print(f"test2() -> {test2()}")
-#print(f"test3() -> {test3()}")
-
 
 def do_part_one():
     c = Combination()
     m = c.read("input.txt")
-    print(f"day 1 part 1 password = {c.password}")
+    print(f"day 1 part 1: password = {c.password}")
 
+
+class Combination_0x434C49434B(Combination):
+
+    def __repr__(self) -> str:
+        return f"Combination_0x434C49434B(password = {self.password}, position = {self.position}, modulus = {self.modulus})"
+
+    def rotate(self, ticks : int) -> int:
+        preposition = self.position + ticks
+        if preposition == 0:
+            self.password += 1
+        if preposition < 0 and self.position != 0:
+            self.password += 1
+        self.password += int(abs(preposition) / self.modulus)
+        self.position = preposition % self.modulus
+        return self.position
+
+
+def test4() -> bool:
+    c4 = Combination_0x434C49434B()
+    for cmd in ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]:
+        m = c4.parse(cmd)
+    return c4.password == 6 and c4.position == 32 and c4.modulus == 100
+
+def do_part_two():
+    c = Combination_0x434C49434B()
+    m = c.read("input.txt")
+    print(f"day 1 part 2: password = {c.password}")
+
+
+print(f"test1() -> {test1()}")
+print(f"test2() -> {test2()}")
+print(f"test3() -> {test3()}")
+print(80 * '-')
 do_part_one()
-
-
-
-
+print(80 * '=')
+print(f"test4() -> {test4()}")
+print(80 * '-')
+do_part_two()
