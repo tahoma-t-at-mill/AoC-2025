50b486b325223108a7bfb4589a38d031d151c946
Tahoma Toelkes <tahoma.t@chewielabs.com>
2025-12-06 04:20:25 -0800

    work in progress

---
+++ day/2/code.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# <https://adventofcode.com/2025/day/2>
+
+def do_part_one():
+    sum = 0
+    print(f"day 2 part 1: What do you get if you add up all of the invalid IDs?\t{sum}")
+
+do_part_one()
