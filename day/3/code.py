5f50861a2b867acd4289dc6a6bc2668bba4c8c39
Tahoma Toelkes <tahoma.t@chewielabs.com>
2025-12-09 07:31:09 -0800

    day 3 part 2

--- day/3/code.py
+++ day/3/code.py
@@ -2,13 +2,13 @@
 
 # <https://adventofcode.com/2025/day/2>
 
-class Bank:
+class Bank1:
 
     def __init__(self, batteries : str):
         self.batteries = batteries
 
     def __repr__(self) -> str:
-        return f"Bank(batteries = {self})"
+        return f"Bank1(batteries = {self})"
 
     def __str__(self) -> str:
         return f"{self.batteries}"
@@ -27,25 +27,73 @@ class Bank:
                 idx0 = j
         return val1 * 10 + val0
 
-def validate():
-    example = [
-        "987654321111111",
-        "811111111111119",
-        "234234234234278",
-        "818181911112111"]
-    banks = [Bank(e) for e in example]
-    for b in banks:
-        print(f"{b} -> {b.max_joltage()}")
 
-#validate()
+example = [
+    "987654321111111",
+    "811111111111119",
+    "234234234234278",
+    "818181911112111"]
+
+def validate(banks):
+    for b in banks:
+        print(f"{repr(b)} -> {b.max_joltage()}")
+    total = 0
+    for b in banks:
+        max_joltage = b.max_joltage()
+        total += max_joltage
+        print(f"{repr(b)} -> {max_joltage}")
+    print(f"\t= {total}")
+
+validate([Bank1(e) for e in example])
 
     
 def day_3_part_1():
     total = 0
     with open("input.txt", 'r') as f:
         for line in f:
-            b = Bank(line.strip())
+            b = Bank1(line.strip())
             total += b.max_joltage()
     print(f"day 3 part 1: what is the total output joltage? -> {total}")
 
 day_3_part_1()
+
+    
+class Bank2(Bank1):
+
+    def __repr__(self) -> str:
+        return f"Bank2(batteries = {self})"
+
+    def pick_max(self, start_idx : int, leave_n : int) -> (int, int):
+        val = 0
+        idx = 0
+        end_idx = len(self.batteries) - leave_n
+        for i in range(start_idx, end_idx):
+            digit = int(self.batteries[i])
+            if digit > val:
+                val = digit
+                idx = i
+        return (val, idx)
+
+    def max_joltage(self) -> int:
+        total = 0
+        idx = 0
+        for leave_n in reversed(range(0, 12)):
+            total *= 10
+            val, pos = self.pick_max(idx, leave_n)
+            total += val
+            idx = pos + 1
+        return total
+
+
+validate([Bank2(e) for e in example])
+
+
+def day_3_part_2():
+    total = 0
+    with open("input.txt", 'r') as f:
+        for line in f:
+            b = Bank2(line.strip())
+            total += b.max_joltage()
+    print(f"day 3 part 2: what is the new total output joltage? -> {total}")
+
+day_3_part_2()
